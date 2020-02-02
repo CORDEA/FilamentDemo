78e82c81b66a52d24f2695485b1bb2581c83c5fa
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-02 12:51:12 +0900

    define extension

---
+++ app/src/main/java/jp/cordea/filamentdemo/InputStreamExt.kt
@@ -0,0 +1,18 @@
+package jp.cordea.filamentdemo
+
+import java.io.InputStream
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+fun InputStream.readFloat32LE() =
+    ByteBuffer
+        .wrap(ByteArray(4).apply { read(this, 0, 4) })
+        .order(ByteOrder.LITTLE_ENDIAN)
+        .float
+
+fun InputStream.readUIntLE(): Long =
+    (read() and 0xFF or
+            (read() and 0xFF shl 8) or
+            (read() and 0xFF shl 16) or
+            (read() and 0xFF shl 24)
+            ).toLong() and 0xFFFFFFFFL
