ddd3e0dc0e23ec46a231996d559c5a8b5728f619
Yoshihiro Tanaka <contact@cordea.jp>
2020-02-02 14:13:11 +0900

    extract material names

--- app/src/main/java/jp/cordea/filamentdemo/MaterialName.kt
+++ app/src/main/java/jp/cordea/filamentdemo/MaterialName.kt
@@ -1,3 +1,16 @@
 package jp.cordea.filamentdemo
 
-inline class MaterialName(val value: String)
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
+
+inline class MaterialName(val value: String) {
+    companion object {
+        fun from(stream: InputStream) =
+            (0..stream.readUIntLE()).map {
+                val data = ByteArray(stream.readUIntLE())
+                stream.read(data)
+                stream.skip(1)
+                data.toString(StandardCharsets.UTF_8)
+            }.map { MaterialName(it) }
+    }
+}
